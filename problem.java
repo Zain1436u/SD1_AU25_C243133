d3b46fa327913443bc64e2dfd42a43e85ac95951
Zain1436u <zoozo1618@gmail.com>
2025-09-18 00:00:15 +0600

    added problem.java

---
+++ problem.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class problem {
+    public static void main(String[] args) {
+        int a,b,c;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        c = sc.nextInt();
+        b=(a+c)/2;
+        System.out.println(b);
+        
+
+
+    }
+}
