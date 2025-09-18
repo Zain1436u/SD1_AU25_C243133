f1d2ba253f4e5625f5e8359c90f52865f9f337a3
Zain1436u <zoozo1618@gmail.com>
2025-09-18 13:29:59 +0600

    changed problem.java

--- problem.java
+++ problem.java
@@ -6,10 +6,13 @@ public class problem {
         Scanner sc = new Scanner(System.in);
         a = sc.nextInt();
         c = sc.nextInt();
+        if ( (a+c)%2==0) {
         b=(a+c)/2;
         System.out.println(b);
-        
-
-
+        }
+        else {
+            System.out.println("IMPOSSIBLE");
+        }
+        sc.close();
     }
 }
